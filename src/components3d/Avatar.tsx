b5e8b87b0656288631a7c6799ecc166e2934adc9
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2026-08-13 19:50:38 +0900

    コメント追加

--- src/components3d/Avatar.tsx
+++ src/components3d/Avatar.tsx
@@ -13,59 +13,63 @@ export function OtherPlayer({peer}: OtherPlayerProps) {
   const bodyRef = useRef<Group>(null);
 
   useFrame((_, _delta) => {
-    if (avatarRef.current) {
+    if(avatarRef.current && bodyRef.current && headRef.current){
+      // アバター全体には、位置を反映する
       avatarRef.current.position.set(...(peer.profile["position"]));
-    }
-    if (bodyRef.current){
+      // 体パーツには、向きのうち左右方向のみを反映する
       const [x, y, z, w] = peer.profile["orientation"];
       bodyRef.current.rotation.y = Math.atan2(2 * (x * z + w * y), 1 - 2 * (x * x + y * y));
-    }
-    if (headRef.current) {
+      // 頭パーツには、向きを反映する
       headRef.current.quaternion.set(...(peer.profile["orientation"]));
     }
   });
 
   return (
     <group ref={avatarRef} position={peer.profile["position"]}>
-      <group ref={headRef} position={[0, 0.25, 0]}>
-        <mesh>
-          <sphereGeometry args={[0.18, 24, 16]} />
-          <meshStandardMaterial color="#f2c7a5" />
-        </mesh>
-        <mesh position={[0, 0, -0.18]} rotation={[Math.PI / 2, 0, 0]}>
-          <coneGeometry args={[0.045, 0.12, 12]} />
-          <meshStandardMaterial color="#d99a78" />
-        </mesh>
-        <mesh position={[-0.065, 0.04, -0.145]}>
-          <sphereGeometry args={[0.018, 8, 8]} />
-          <meshStandardMaterial color="#1f2937" />
-        </mesh>
-        <mesh position={[0.065, 0.04, -0.145]}>
-          <sphereGeometry args={[0.018, 8, 8]} />
-          <meshStandardMaterial color="#1f2937" />
-        </mesh>
-      </group>
-      <group ref={bodyRef}>
-        <mesh position={[0, -0.18, 0]}>
-          <capsuleGeometry args={[0.18, 0.55, 8, 16]} />
-          <meshStandardMaterial color="#3d6ee8" />
-        </mesh>
-        <mesh position={[-0.24, -0.2, 0]} rotation={[0, 0, 0.25]}>
-          <capsuleGeometry args={[0.055, 0.45, 6, 12]} />
-          <meshStandardMaterial color="#f2c7a5" />
-        </mesh>
-        <mesh position={[0.24, -0.2, 0]} rotation={[0, 0, -0.25]}>
-          <capsuleGeometry args={[0.055, 0.45, 6, 12]} />
-          <meshStandardMaterial color="#f2c7a5" />
-        </mesh>
-        <mesh position={[-0.08, -0.78, 0]}>
-          <capsuleGeometry args={[0.07, 0.55, 6, 12]} />
-          <meshStandardMaterial color="#29324a" />
-        </mesh>
-        <mesh position={[0.08, -0.78, 0]}>
-          <capsuleGeometry args={[0.07, 0.55, 6, 12]} />
-          <meshStandardMaterial color="#29324a" />
-        </mesh>
+      {/* アバター全体 */}
+      <group>
+        {/* 頭パーツ */}
+        <group ref={headRef} position={[0, 0.25, 0]}>
+          <mesh>
+            <sphereGeometry args={[0.18, 24, 16]} />
+            <meshStandardMaterial color="#f2c7a5" />
+          </mesh>
+          <mesh position={[0, 0, -0.18]} rotation={[Math.PI / 2, 0, 0]}>
+            <coneGeometry args={[0.045, 0.12, 12]} />
+            <meshStandardMaterial color="#d99a78" />
+          </mesh>
+          <mesh position={[-0.065, 0.04, -0.145]}>
+            <sphereGeometry args={[0.018, 8, 8]} />
+            <meshStandardMaterial color="#1f2937" />
+          </mesh>
+          <mesh position={[0.065, 0.04, -0.145]}>
+            <sphereGeometry args={[0.018, 8, 8]} />
+            <meshStandardMaterial color="#1f2937" />
+          </mesh>
+        </group>
+        {/* 体パーツ */}
+        <group ref={bodyRef}>
+          <mesh position={[0, -0.18, 0]}>
+            <capsuleGeometry args={[0.18, 0.55, 8, 16]} />
+            <meshStandardMaterial color="#3d6ee8" />
+          </mesh>
+          <mesh position={[-0.24, -0.2, 0]} rotation={[0, 0, 0.25]}>
+            <capsuleGeometry args={[0.055, 0.45, 6, 12]} />
+            <meshStandardMaterial color="#f2c7a5" />
+          </mesh>
+          <mesh position={[0.24, -0.2, 0]} rotation={[0, 0, -0.25]}>
+            <capsuleGeometry args={[0.055, 0.45, 6, 12]} />
+            <meshStandardMaterial color="#f2c7a5" />
+          </mesh>
+          <mesh position={[-0.08, -0.78, 0]}>
+            <capsuleGeometry args={[0.07, 0.55, 6, 12]} />
+            <meshStandardMaterial color="#29324a" />
+          </mesh>
+          <mesh position={[0.08, -0.78, 0]}>
+            <capsuleGeometry args={[0.07, 0.55, 6, 12]} />
+            <meshStandardMaterial color="#29324a" />
+          </mesh>
+        </group>
       </group>
     </group>
   );
